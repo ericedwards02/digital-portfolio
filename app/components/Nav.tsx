c21ee18d8bb2d34624181133f929796ed2418eab
Eric Edwards <eric.edwards02@gmail.com>
2025-01-30 13:16:39 -0800

    fix(style): nav responsiveness

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -16,13 +16,14 @@ function formatTitleIndex(index: number): string {
 
 export default function Nav({ setActivePost }: NavProps) {
   return (
-    <ul className="list-none p-0 m-0 w-[200px]">
+    <ul className="list-none p-0 m-0 min-w-[175px]">
       <li className="text-lg font-bold">portfolio</li>
       {posts.map((post, index) => (
         <li
           key={post.id}
           onClick={() => setActivePost(post)}
-          className="text-lg text-gray-500 transition-all duration-250 cursor-pointer mb-2.5 hover:text-xl hover:text-white hover:font-bold"
+          className="text-lg text-gray-500 transition-all duration-250 cursor-pointer mb-2.5 hover:text-xl
+          hover:text-[var(--subheaders)] hover:font-bold"
         >
           {formatTitleIndex(index)} {post.title}
         </li>
