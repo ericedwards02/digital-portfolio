f08f937ba1b0a45f9b178456abadbf29d1b175f6
Eric Edwards <eric.edwards02@gmail.com>
2025-01-30 13:23:49 -0800

    chore: analytics

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,27 @@ import { useState } from "react";
 import Nav from "./components/Nav";
 import ContentSection from "./components/ContentSection";
 import { posts } from "./data/posts";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   const [activePost, setActivePost] = useState(posts[0]);
 
   return (
-    <div className="font-sans max-w-[1600px] mx-auto p-5">
-      <h1 className="mb-0 text-center font-bold text-xl">
-        Eric Edwards &mdash; dev and design
-      </h1>
-      <h2 className="mt-0 text-center text-lg">
-        &quot;Next time is next time, now is now!&quot;
-        <i> - Hirayama (Perfect Days, 2023)</i>
-      </h2>
-      <div className="flex gap-10">
-        <Nav setActivePost={setActivePost} />
-        <ContentSection activePost={activePost} />
+    <>
+      <Analytics />
+      <div className="font-sans max-w-[1600px] mx-auto p-5">
+        <h1 className="mb-0 text-center font-bold text-xl">
+          Eric Edwards &mdash; dev and design
+        </h1>
+        <h2 className="mt-0 text-center text-lg">
+          &quot;Next time is next time, now is now!&quot;
+          <i> - Hirayama (Perfect Days, 2023)</i>
+        </h2>
+        <div className="flex gap-10">
+          <Nav setActivePost={setActivePost} />
+          <ContentSection activePost={activePost} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
