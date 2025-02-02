051b9e64acbba64227d63e8206187c846e6aa638
Eric Edwards <eric.edwards02@gmail.com>
2025-02-02 12:15:22 -0800

    fix: removed light mode preference
    May add a light mode theme, but ignoring for now.

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-ctp-base`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-ctp-base`}
       >
         {children}
       </body>
