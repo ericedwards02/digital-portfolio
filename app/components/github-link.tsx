2f3cb56c07bbfca1422fa9439477d679d8ef6340
Eric Edwards <eric.edwards02@gmail.com>
2025-03-11 10:07:21 -0700

    style: git link

--- app/components/github-link.tsx
+++ app/components/github-link.tsx
@@ -1,10 +1,16 @@
 import { icons } from "./icons";
 
-export default function GithubLink(props: { link: string }) {
+export default function GithubLink(props: { link: string; text?: string }) {
   return (
-    <div id="project-link">
-      <a href={props.link} className="inline-flex gap-1 font-mono">
-        {icons.github} Link to GitHub
+    <div
+      id="project-link"
+      className="bg-ctp-mantle size-fit px-3 py-1 flex transform transition duration-125 hover:scale-90 rounded-md"
+    >
+      <a
+        href={props.link}
+        className="inline-flex gap-1 font-mono place-self-center"
+      >
+        {icons.github} {props.text}
       </a>
     </div>
   );
